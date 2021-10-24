6bddbfcd1545175e81a328b44d58800659555fdf
Фомин Никита <asdf65896@gmail.com>
2021-10-24 22:26:05 +0200

    добавил рекоммендации

--- film_recommendation.php
+++ film_recommendation.php
@@ -1,13 +1,15 @@
 <?php
 declare(strict_types=1);
-require "../movies.php";
- $moviesarray=include $movies.php;
-$age= readline("");
-    if (is_numeric($age)==true && (int)$age<100 && (int)$age>0) {
-        foreach (array($moviesarray)as $movie) {
-            if ($moviesarray['age restriction'] < age or $moviesarray['age restriction'] = $age) {
-                echo $moviesarray['title'];
-                echo "</br>";
+require ('movies.php');
+
+$age= readline("\n");
+
+    if (is_numeric($age) && (int)$age>=0) {
+        foreach ($movies as $movie) {
+            if ($movie['age restriction'] <= age) {
+                echo $movie['title'];
+                echo "\n";
+
             }
         }
     }
